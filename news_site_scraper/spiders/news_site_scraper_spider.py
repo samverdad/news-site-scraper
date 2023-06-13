cdff0f78da9fda8a8f8d73761411218b18cc1a47
Sarah Verdad <sam.verdad@gmail.com>
2023-06-13 16:15:06 +0800

    Update scrapers 1 and 3

--- news_site_scraper/spiders/news_site_scraper_spider.py
+++ news_site_scraper/spiders/news_site_scraper_spider.py
@@ -2,12 +2,15 @@ from urllib.parse import urlencode
 from dotenv import load_dotenv
 import scrapy
 import os
+import json
+
 
 load_dotenv()
 PROXY_URL = os.environ["PROXY_URL"]
 API_KEY = os.environ["API_KEY"]
 
 DOMAIN_NAME = 'https://www.freemalaysiatoday.com'
+API_URL = 'https://cms.freemalaysiatoday.com/graphql'
 
 def get_proxy_url(url):
     payload = {'api_key': API_KEY, 'url': url}
@@ -18,28 +21,57 @@ class NewsSiteScraperSpider(scrapy.Spider):
     name = 'newssitescraperspider'
 
     def start_requests(self):
-        start_url = DOMAIN_NAME + '/?s=ramadan+bazaar&c=all'
-        yield scrapy.Request(url=get_proxy_url(start_url), callback=self.parse)
+        config = self.req_config(curr_offset=0)
+        yield scrapy.FormRequest(url=get_proxy_url(config['api_url']), method="POST", headers=config['headers'], body=config['body'], callback=self.parse)
 
     def parse(self, response):
-        selector = 'article a.summary-title-link::attr(href)'
-        for item_url in response.css(selector).getall():
-            yield scrapy.Request(url=get_proxy_url(DOMAIN_NAME + item_url), callback=self.scrape, cb_kwargs={'url': item_url})
+        self.crawl(response)
 
-        # next_page_url = response.css('').get()
-        
-        # if next_page_url:
-        #     yield scrapy.Request(get_proxy_url(next_page_url), callback=self.parse)
+        total = self.get_total_pages(response)
+
+        if total > 10:
+            for offset in range(10, total, 10):
+                config = self.req_config(curr_offset=offset)
+                yield scrapy.FormRequest(url=get_proxy_url(config['api_url']), method="POST", headers=config['headers'], body=config['body'], callback=self.crawl)
 
     def crawl(self, response):
-        yield response
+        body = json.loads(response.body)
+        edges = body['data']['posts']['edges']
 
-    def scrape(self, response, url):
+        for item in edges:
+            item_url = DOMAIN_NAME + item['node']['uri']
+            item_date = item['node']['dateGmt']
+            yield scrapy.Request(url=get_proxy_url(item_url), callback=self.scrape, cb_kwargs={'url': item_url, 'datetime': item_date})
+
+    def scrape(self, response, url, datetime):
         yield {
             'title': response.css('h1.Page__PostTitleH1-sc-1auxjzz-0::text').get(),
             'author': response.css('span.author a.Style__StyledAnchor-sc-kwuyeg-0::text').get(),
             'location': '',
-            'datetime': response.css('div').xpath('./time/@datetime').get(), # to fix
+            'datetime': datetime,
             'content': response.css('div.Content__StyledDiv-sc-1n9vywj-0 p::text').getall(),
             'url': DOMAIN_NAME + url,
         }
+
+    def req_config(self, curr_offset):
+        offset = curr_offset
+        form_data = {
+            "query": '\n    query searchPosts {\n      \n      posts(\n        where: {\n          taxQuery: { taxArray: [{ terms: [\"top-news\",\"top-bm\",\"business\",\"local-business\",\"world-business\",\"nation\",\"sabahsarawak\",\"tempatan\",\"pandangan\",\"dunia\",\"column\",\"editorial\",\"letters\",\"fmt-worldviews\",\"world\",\"south-east-asia\",\"property\",\"sports\",\"football\",\"badminton\",\"motorsports\",\"tennis\",\"leisure\",\"top-lifestyle\",\"automotive\",\"simple-stories\",\"travel\",\"food\",\"entertainment\",\"money\",\"health\",\"pets\",\"education\"], operator: IN, taxonomy: CATEGORY, field: SLUG }], relation: AND },\nsearch: \"ramadan bazaar\",\noffsetPagination: {offset: ' + str(offset) + ', size: 10}\n        },\n        first: null,\n        after: \"\",\n        before: \"\",\n        last: null\n      ) {\n        \n        \n          pageInfo {\n            offsetPagination {\n              hasMore\n              hasPrevious\n              total\n            }\n          }\n        \n        edges {\n          node {\n            id\n            databaseId\n            slug\n            title\n            dateGmt\n            uri\n            excerpt\n            featuredImage {\n              node {\n                mediaItemUrl\n                \n              }\n            }\n            \n            \n    categories(first: 1, where: {\n      \n    }) {\n      nodes {\n        id\n        slug\n        name\n        uri\n      }\n    }\n  \n            \n          }\n        }\n      }\n    \n    }\n  ',
+        }
+        body = json.dumps(form_data)
+        headers = {
+            "Content-Type": "application/json",
+        }
+
+        return {
+            'api_url': API_URL,
+            'offset': offset,
+            'form_data': form_data,
+            'body': body,
+            'headers': headers,
+        }
+    
+    def get_total_pages(self, response):
+        body = json.loads(response.body)
+        total_pages = body['data']['posts']['pageInfo']['offsetPagination']['total']
+        return total_pages
--- news_site_scraper/spiders/news_site_scraper_spider3.py
+++ news_site_scraper/spiders/news_site_scraper_spider3.py
@@ -2,12 +2,15 @@ from urllib.parse import urlencode
 from dotenv import load_dotenv
 import scrapy
 import os
+import json
+
 
 load_dotenv()
 PROXY_URL = os.environ["PROXY_URL"]
 API_KEY = os.environ["API_KEY"]
 
 DOMAIN_NAME = 'https://www.malaysianow.com'
+API_URL = 'https://api.malaysianow.com/graphql'
 
 def get_proxy_url(url):
     payload = {'api_key': API_KEY, 'url': url}
@@ -18,21 +21,26 @@ class NewsSiteScraperSpider3(scrapy.Spider):
     name = 'newssitescraperspider3'
 
     def start_requests(self):
-        start_url = DOMAIN_NAME + '/search?query=ramadan+bazaar'
-        yield scrapy.Request(url=get_proxy_url(start_url), callback=self.parse)
+        config = self.req_config(curr_page=0)
+        yield scrapy.FormRequest(url=get_proxy_url(config['api_url']), method="POST", headers=config['headers'], body=config['body'], callback=self.parse)
 
     def parse(self, response):
-        selector = 'div.group.flex.flex-col.overflow-hidden.rounded-md.border-2.border-gray-100 a::attr(href)'
-        for item_url in response.css(selector).getall():
-            yield scrapy.Request(url=get_proxy_url(item_url), callback=self.scrape, cb_kwargs={'url': item_url})
+        self.crawl(response)
 
-        # next_page_url = response.css('').get()
-        
-        # if next_page_url:
-        #     yield scrapy.Request(get_proxy_url(next_page_url), callback=self.parse)
+        total_pages = self.get_total_pages(response)
+
+        if total_pages > 1:
+            for page in range(1, total_pages):
+                config = self.req_config(curr_page=page)
+                yield scrapy.FormRequest(url=get_proxy_url(config['api_url']), method="POST", headers=config['headers'], body=config['body'], callback=self.crawl)
 
     def crawl(self, response):
-        yield response
+        body = json.loads(response.body)
+        articles = body['data']['articlesByQuery']['articles']['articles']
+
+        for item in articles:
+            item_url = DOMAIN_NAME + item['url']
+            yield scrapy.Request(url=get_proxy_url(item_url), callback=self.scrape, cb_kwargs={'url': item_url})
 
     def scrape(self, response, url):
         yield {
@@ -43,3 +51,33 @@ class NewsSiteScraperSpider3(scrapy.Spider):
             'content': response.css('div#content-wrap p::text').getall(),
             'url': url,
         }
+
+    def req_config(self, curr_page):
+        page = curr_page
+        form_data = {
+            "operationName": "ARTICLES_BY_QUERY",
+            "variables": {
+                "query": "ramadan bazaar",
+                "locale": "EN",
+                "page": page
+            },
+            "extensions": {},
+            "query": "query ARTICLES_BY_QUERY($locale: LanguageType!, $query: String!, $page: Int) {\n  articlesByQuery(locale: $locale, query: $query, page: $page) {\n    query\n    articles {\n      summary {\n        totalItems\n        fromItems\n        toItems\n        currentPage\n        totalPages\n        __typename\n      }\n      articles {\n        title\n        excerpt\n        label {\n          name\n          slug\n          __typename\n        }\n        featuredImage {\n          url\n          altText\n          __typename\n        }\n        url\n        publishedAt\n        updatedAt\n        __typename\n      }\n      __typename\n    }\n    mostReadArticles {\n      title\n      excerpt\n      label {\n        name\n        slug\n        __typename\n      }\n      featuredImage {\n        url\n        altText\n        __typename\n      }\n      url\n      publishedAt\n      updatedAt\n      __typename\n    }\n    __typename\n  }\n}"
+        }
+        body = json.dumps(form_data)
+        headers = {
+            "Content-Type": "application/json",
+        }
+
+        return {
+            'api_url': API_URL,
+            'page': page,
+            'form_data': form_data,
+            'body': body,
+            'headers': headers,
+        }
+    
+    def get_total_pages(self, response):
+        body = json.loads(response.body)
+        total_pages = body['data']['articlesByQuery']['articles']['summary']['totalPages']
+        return total_pages
